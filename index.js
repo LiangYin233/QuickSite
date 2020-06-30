fa301af340393ab02d46b6157d6adff74675edfb
LiangYin <liangyingugugu@163.com>
2020-06-30 20:30:52 +0800

    alpha4

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ var fs = require('fs-extra');
 var ejs = require('ejs');
 var MarkdownIt = require('markdown-it');
 const readline = require('readline');
+var jsonxml = require('jsontoxml');
 md = new MarkdownIt();
 
 var data = {
@@ -28,14 +29,18 @@ if (process.argv[2] == "build") {
         var templatefiles = new Array(), postfiles = new Array();
         templatefiles = fs.readdirSync(process.cwd() + "/template/"), postfiles = fs.readdirSync(process.cwd() + "/posts/");
 
-        //去JSON注释 正则源于:CSDN - 静思映雪
-        var reg = /("([^\\\"]*(\\.)?)*")|('([^\\\']*(\\.)?)*')|(\/{2,}.*?(\r|\n))|(\/\*(\n|.)*?\*\/)/g, json = fs.readFileSync(process.cwd() + "/config.json", "utf8");
+        // 去JSON注释 正则源于:CSDN - 静思映雪
+        var reg = /("([^\\\"]*(\\.)?)*")|('([^\\\']*(\\.)?)*')|(\/{2,}.*?(\r|\n))|(\/\*(\n|.)*?\*\/)/g, json = fs.readFileSync(process.cwd() + "/config.json", "utf8"),sitejson = fs.readFileSync(process.cwd() + "/site.json", "utf8");
         json = json.replace(reg, function (word) {
             return /^\/{2,}/.test(word) || /^\/\*/.test(word) ? "" : word;
         });
         json = JSON.parse(json);
+        sitejson = sitejson.replace(reg, function (word) {
+            return /^\/{2,}/.test(word) || /^\/\*/.test(word) ? "" : word;
+        });
+        sitejson = JSON.parse(sitejson);
 
-        //赋值
+        // 赋值
         for (let i = 0; i < postfiles.length; i++) {
             data.contents[i] = md.render(fs.readFileSync(process.cwd() + "/posts/" + postfiles[i], "utf8"))
             data.filename[i] = String(postfiles[i]).split(".")[0].split(";")[0];
@@ -56,7 +61,7 @@ if (process.argv[2] == "build") {
             }
         );
 
-        //写index文件
+        // 写index文件
         fs.writeFile(process.cwd() + '/public/' + 'index.html', html, function (err) {
             if (err) {
                 return console.error("QuickSite Error:" + err);
@@ -65,7 +70,7 @@ if (process.argv[2] == "build") {
 
         //渲染Posts页面
         for (let i = 0; i < data.filename.length; i++) {
-            if (i == 0 && (data.filename.length == 2)) {//判断是否是第一个博文且有>两篇文章的情况
+            if (i == 0 && (data.filename.length == 2)) {// 判断是否是第一个博文且有>两篇文章的情况
                 var data_posts = {
                     content: data.contents[i],
                     title: data.titles[i],
@@ -77,7 +82,7 @@ if (process.argv[2] == "build") {
                     }
                 };
             }
-            else if (i != data.filename.length - 1 && i > 0) {//既不是第一篇,又不是最后一篇文章的情况
+            else if (i != data.filename.length - 1 && i > 0) {// 既不是第一篇,又不是最后一篇文章的情况
                 var data_posts = {
                     content: data.contents[i],
                     title: data.titles[i],
@@ -93,7 +98,7 @@ if (process.argv[2] == "build") {
                     }
                 };
             }
-            else if (i == data.filename.length - 1 && (data.filename.length > 1)) { //最后一篇文章的情况
+            else if (i == data.filename.length - 1 && (data.filename.length > 1)) { // 最后一篇文章的情况
                 var data_posts = {
                     content: data.contents[i],
                     title: data.titles[i],
@@ -105,7 +110,7 @@ if (process.argv[2] == "build") {
                     }
                 };
             }
-            else { //只有一篇文章的情况
+            else { // 只有一篇文章的情况
                 var data_posts = {
                     content: data.contents[i],
                     title: data.titles[i],
@@ -128,7 +133,7 @@ if (process.argv[2] == "build") {
             });
         }
 
-        //渲染自定义页面
+        // 渲染自定义页面
         for (let i = 0; i < fs.readdirSync(process.cwd() + "/template/").length; i++) {
             if (templatefiles[i] != "index.ejs" && templatefiles[i] != "posts.ejs") {
                 compiled = ejs.compile(fs.readFileSync(process.cwd() + '/template/' + templatefiles[i], 'utf8'));
@@ -145,6 +150,25 @@ if (process.argv[2] == "build") {
                 });
             }
         }
+
+        // 生成Sitemap
+        var Sitemap = [];
+        for (let i = 0; i < data.filename.length; i++) {
+            Sitemap.push({
+                "url": {
+                    "loc": sitejson.url + data.filename[i] + ".html",
+                    "lastmod": new Date(data.birthtime[i]).toISOString()
+                }
+            });
+        }
+        fs.writeFile(process.cwd() + '/public/' + 'sitemap.xml','<?xml version="1.0" encoding="UTF-8"?>' + jsonxml({
+            "urlset": Sitemap
+        }), function (err) {
+            if (err) {
+                return console.error("QuickSite Error:" + err);
+            }
+        });
+
         console.log("Static pages have been built")
     }
     catch (err) {
@@ -152,12 +176,12 @@ if (process.argv[2] == "build") {
     }
 }
 else if (process.argv[2] == null) {
-    console.log("QuickSite Alpha3");
+    console.log("QuickSite Alpha4");
     console.log("Quickly generate efficient, safe and beautiful static websites")
     console.log("Usage: qs [command]");
-    console.log("Command:\n  build - Generate static pages\n  new - Create new post");
-    console.log("What has been updated in QuickSite Alpha3?");
-    console.log("- Updated posts object\n- Optimize code")
+    console.log("Command:\n  build - Generate static pages\n  new - Create new post\n  clean - Delete the public folder");
+    console.log("What has been updated in QuickSite Alpha4?");
+    console.log("- Support sitemap\n- Add public folder delete function")
 }
 else if (process.argv[2] == "new") {
     const rl = readline.createInterface({
@@ -177,6 +201,10 @@ else if (process.argv[2] == "new") {
     rl.on("close", function () {
         console.log("A new post has been created")
     });
+}
+else if (process.argv[2] == "clean") {
+    fs.removeSync(process.cwd() + "/public")
+    console.log("public folder deleted")
 }
 else {
     console.info("Unknown commands and parameters");
